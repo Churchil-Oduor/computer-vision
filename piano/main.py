159b4ad4cc0d0dc4db3277a828647de4d2ef4dc4
Churchil-Oduor <churchilokechoduor@gmail.com>
2025-06-02 14:11:07 +0300

    Started the piano project

---
+++ piano/main.py
@@ -0,0 +1,63 @@
+import cv2 as cv
+from utils import RectangleDetector
+from utils import HandTracker
+from utils import point_inside_rectangle
+
+def main():
+    feed = cv.VideoCapture(0)
+    rect_detector = RectangleDetector()
+    hand_tracker = HandTracker()
+
+    rectangles = []
+    rectangles_Detected = False
+
+    while True: 
+        isTrue, frame = feed.read()
+        if not isTrue:
+            break
+
+        frame = cv.flip(frame, 1)
+
+        h, w, _ = frame.shape
+        key = cv.waitKey(1) & 0xff
+
+        # detecting rectangles when the r key is pressed
+        if key == ord('r'):
+            rectangles = rect_detector.detect_rectangles(frame)
+            rectangles_Detected = True
+            print(f"Detected {len(rectangles)} rectangle(s)")
+
+
+        # drawing stored rectangles if detected
+        if rectangles_Detected:
+            for rect in rectangles:
+                cv.drawContours(frame, [rect], -1, (0, 255, 0), 3)
+
+        hand_results = hand_tracker.process(frame)
+        if hand_results.multi_hand_landmarks:
+            for hand_landmarks in hand_results.multi_hand_landmarks:
+                hand_tracker.draw_landmarks(frame, hand_landmarks)
+
+                index_finger_tip = hand_landmarks.landmark[8]
+                x_tip = int(index_finger_tip.x * w)
+                y_tip = int(index_finger_tip.y * h)
+
+                cv.circle(frame, (x_tip, y_tip), 8, (255, 0, 0), -1)
+
+                if rectangles_Detected:
+                    for i, rect in enumerate(rectangles):
+                        if point_inside_rectangle((x_tip, y_tip), rect):
+                            cv.putText(frame, f"Key {i+1} pressed", (10, 40 + i* 30), cv.FONT_HERSHEY_SIMPLEX, 0.8, (0,0,255), 2),
+                            cv.drawContours(frame, [rect], -1, (0,0, 255), 3)
+
+        cv.imshow("Piano key detection", frame)
+
+        if key == ord('q'):
+            break
+
+    feed.release()
+    cv.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
---
+++ piano/utils.py
@@ -0,0 +1,57 @@
+import cv2 as cv
+import mediapipe as mp
+
+class RectangleDetector:
+    """
+    Detects the drawn keys
+
+    """
+
+    def __init__(self):
+        pass
+
+    def is_rectangle(self, contour):
+        """
+        checks to see if the figure is a rectangle
+
+        contour: received contour from frame.
+        Returns: a tuple containing a bool and approximation of the 
+        rectangle
+        """
+        epsilon = 0.02 * cv.arcLength(contour, True)
+        approx = cv.approxPolyDP(contour, epsilon, True)
+        return len(approx) == 4 and cv.isContourConvex(approx), approx
+
+    def detect_rectangles(self, image):
+        gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+        blurred = cv.GaussianBlur(gray, (5, 5), 0)
+        edges = cv.Canny(blurred, 50, 150)
+
+        contours, _ = cv.findContours(edges, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+        rectangles = []
+
+        for cnt in contours:
+            frame_check, approx = self.is_rectangle(cnt)
+            if frame_check:
+                rectangles.append(approx)
+        return rectangles
+
+def point_inside_rectangle(point, polygon):
+    return cv.pointPolygonTest(polygon, point, False) >= 0
+
+
+class HandTracker:
+    def __init__(self, max_num_hands=1):
+        self.mp_hands = mp.solutions.hands
+        self.hands = self.mp_hands.Hands(static_image_mode=False, max_num_hands=max_num_hands)
+        self.mp_draw = mp.solutions.drawing_utils
+
+    def process(self, frame):
+        frame_rgb = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+        return self.hands.process(frame_rgb)
+
+    def draw_landmarks(self, frame, hand_landmarks):
+        self.mp_draw.draw_landmarks(frame, hand_landmarks, self.mp_hands.HAND_CONNECTIONS)
+
+
+
